ebb1d2d3bafca7e83559f0bb1a10425190b3ef5c
DanielMora <daniel.santiago.mora@correounivalle.edu.co>
2025-12-08 17:37:22 -0500

    Se agrega el menu del juego

---
+++ src/main/java/com/example/batallanavalgame/controllers/MenuController.java
@@ -0,0 +1,16 @@
+package com.example.batallanavalgame.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MenuController {
+    @FXML
+    void cargarPartida(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciarPartida(ActionEvent event) {
+
+    }
+}
