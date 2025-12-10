8c08f06429a61fbca551cb5be4fe4762734f443f
DanielMora <daniel.santiago.mora@correounivalle.edu.co>
2025-12-10 12:39:30 -0500

    Se agrega la primera escena del juego

---
+++ src/main/java/com/example/batallanavalgame/controllers/UbicarFlotaController.java
@@ -0,0 +1,21 @@
+package com.example.batallanavalgame.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class UbicarFlotaController {
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    void continuar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verFlotaEnemiga(ActionEvent event) {
+
+    }
+}
