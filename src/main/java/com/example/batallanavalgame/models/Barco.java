078e21c341f74387afe215e68bb81ffa4622b4ef
DanielMora <daniel.santiago.mora@correounivalle.edu.co>
2025-12-14 19:48:40 -0500

    Se genera la documentacion del codigo fuente (Javadoc)

--- src/main/java/com/example/batallanavalgame/models/Barco.java
+++ src/main/java/com/example/batallanavalgame/models/Barco.java
@@ -2,16 +2,32 @@ package com.example.batallanavalgame.models;
 
 import java.io.Serializable;
 
+/**
+ * Representa un barco dentro del juego Batalla Naval
+ */
 public class Barco implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    /** Tipo del barco */
     private String tipo;
+    /** Numero de celdas que ocupa el barco */
     private int size;
-    private String orientacion; // "HORIZONTAL" o "VERTICAL"
+    /** Orientación del barco: "HORIZONTAL" o "VERTICAL". */
+    private String orientacion;
+    /** Fila inicial donde se coloca el barco. */
     private int initialRow;
+    /** Columna inicial donde se coloca el barco. */
     private int initialCol;
+    /** Cantidad de golpes que ha recibido el barco. */
     private int golpes = 0;
 
+    /**
+     * Constructor que crea un barco con todos sus atributos
+     * @param tipo tipo del barco
+     * @param size tamaño del barco en celdas
+     * @param orientacion orientación del barco ("HORIZONTAL" o "VERTICAL")
+     * @param initialRow fila inicial en el tablero
+     * @param initialCol columna inicial en el tablero
+     */
     public Barco(String tipo, int size, String orientacion, int initialRow, int initialCol) {
         this.tipo = tipo;
         this.size = size;
@@ -19,7 +35,12 @@ public class Barco implements Serializable {
         this.initialRow = initialRow;
         this.initialCol = initialCol;
     }
-
+    /**
+     * Obtiene el tamaño de un barco según su tipo.
+     *
+     * @param tipo tipo del barco
+     * @return tamaño correspondiente al tipo del barco
+     */
     public static int getSizeByType(String tipo) {
         switch (tipo) {
             case "Portaaviones": return 4;
@@ -30,8 +51,15 @@ public class Barco implements Serializable {
         }
     }
 
+    /**
+     * Registra golpe recibido a un barco
+     */
     public void agregarGolpe() { golpes++; }
 
+    /**
+     * Verifica si el barco ha sido hundido.
+     * @return true si el numero de golpes es mayor o igual al tamaño del barco, false en caso contrario
+     */
     public boolean estaHundido() { return golpes >= size; }
 
     public String getTipo() { return tipo; }
